408b0d947803eb97e3924e35d001072336eca885
KoukiIijima <lle0nonnndebo@gmail.com>
2020-08-30 14:53:25 +0900

    end of section 5

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,19 +1,31 @@
 import React from 'react'
-import { Segment, Menu } from 'semantic-ui-react'
+import { Segment, Menu, Container, Button } from 'semantic-ui-react'
 
-export const NavBar = () => {
+interface IProps {
+    openCreateForm: () => void;
+}
+
+export const NavBar: React.FC<IProps> = ({ openCreateForm }) => {
     return (
         <Segment inverted>
-            <Menu inverted secondary>
-                <Menu.Item
-                    name='home'
-                />
-                <Menu.Item
-                    name='messages'
-                />
-                <Menu.Item
-                    name='friends'
-                />
+            <Menu fixed="top" inverted>
+                <Container>
+                    <Menu.Item
+                        header
+                        name='home'
+                    >
+                        <img src="/assets/logo.png" style={{ marginRight: '10px' }} alt="" />
+                        Reactivities
+                    </Menu.Item>
+                    <Menu.Item
+                        name='friends'
+                    />
+                    <Menu.Item
+                        name='messages'
+                    >
+                        <Button onClick={openCreateForm} content="Create Activities" />
+                    </Menu.Item>
+                </Container>
             </Menu>
         </Segment>
     )
